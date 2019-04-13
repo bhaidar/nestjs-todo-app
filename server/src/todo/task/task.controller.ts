a9e9eb326bfb5acae9349ac9602a1f5e7b3fb8b1
Bilal Haidar <bhaidar@gmail.com>
2019-04-13 11:27:31 +0300

    replace TaskDto by TaskCreateDto in the Task.create() (#3)

--- server/src/todo/task/task.controller.ts
+++ server/src/todo/task/task.controller.ts
@@ -11,6 +11,7 @@ import {
 import { TaskService } from './task.service';
 import { TaskListDto } from '../dto/task.list.dto';
 import { TaskDto } from '../dto/task.dto';
+import { TaskCreateDto } from '@todo/dto/task.create.dto';
 
 @Controller('api/tasks')
 export class TaskController {
@@ -31,7 +32,7 @@ export class TaskController {
   @UsePipes(new ValidationPipe())
   async create(
     @Param('id') todo: string,
-    @Body() taskDto: TaskDto,
+    @Body() taskDto: TaskCreateDto,
   ): Promise<TaskDto> {
     return await this.taskService.createTask(todo, taskDto);
   }
