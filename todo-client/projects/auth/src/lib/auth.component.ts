0f493f06480a768cb7d1fe5bcaf87fdb377e615c
Bilal Haidar <bhaidar@gmail.com>
2019-08-14 18:28:53 +0300

    configure tslint and prettier to work together

---
+++ todo-client/projects/auth/src/lib/auth.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-auth',
+  template: `
+    <p>
+      auth works!wq jo rjeoif jwiorj weiorjwe oirjwe orijwe riowejr oiwejr
+      oiwejr wioejr ewiorj ewioj
+    </p>
+  `,
+  styles: []
+})
+export class AuthComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
