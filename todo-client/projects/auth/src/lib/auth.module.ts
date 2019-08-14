07460b0e81c2073cc4cb62139ab967fc781a2652
Bilal Haidar <bhaidar@gmail.com>
2019-08-14 18:28:53 +0300

    configure tslint and prettier to work together

---
+++ todo-client/projects/auth/src/lib/auth.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [],
+  exports: [AuthComponent]
+})
+export class AuthModule {}
---
+++ todo-client/projects/auth/src/lib/auth.service.ts
@@ -0,0 +1,53 @@
+/**
+ * Based on
+ * https://github.com/cornflourblue/angular-7-jwt-authentication-example
+ */
+
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+export interface ApplicationUser {
+  access_token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<ApplicationUser>;
+  public currentUser: Observable<ApplicationUser>;
+
+  constructor(private readonly http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<ApplicationUser>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): ApplicationUser {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>('/auth/login', { username, password }).pipe(
+      map(user => {
+        // login successful if there's a jwt token in the response
+        if (user && user.access_token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+
+        return user;
+      })
+    );
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
